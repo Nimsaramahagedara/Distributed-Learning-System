febb17dc288f4f39595c65a4100fdf4cc2d25ad2
nimsaramahagedara <nimsaramahagedara@gmail.com>
2024-05-13 03:34:26 +0530

    fix

--- client/src/components/listItems.jsx
+++ client/src/components/listItems.jsx
@@ -59,7 +59,7 @@ export const learnerListItems = (
       <ListItemText primary="Home" />
     </ListItemButton>
   </Link>
-  <Link to={'/search'}>
+  <Link to={'./search'}>
     <ListItemButton>
       <ListItemIcon>
         <DashboardIcon />
@@ -67,7 +67,7 @@ export const learnerListItems = (
       <ListItemText primary="Search" />
     </ListItemButton>
   </Link>
-  <Link to={'/my-learning'}>
+  <Link to={'./my-learning'}>
     <ListItemButton>
       <ListItemIcon>
         <DashboardIcon />
@@ -83,7 +83,7 @@ export const learnerListItems = (
       <ListItemText primary="Profile" />
     </ListItemButton>
   </Link>
-  <Link to={'/transactions'}>
+  <Link to={'./transactions'}>
     <ListItemButton>
       <ListItemIcon>
         <DashboardIcon />
