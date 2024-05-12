4a237d1b4a04e42538078f53b1d0d9c9ba337787
nimsaramahagedara <nimsaramahagedara@gmail.com>
2024-05-12 18:28:21 +0530

    fix-router-json-bpdy

--- server/gateway/index.js
+++ server/gateway/index.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import morgan from 'morgan'
 import dotenv from 'dotenv'
 dotenv.config()
-import { createProxyMiddleware } from 'http-proxy-middleware'
+import { createProxyMiddleware,fixRequestBody } from 'http-proxy-middleware'
 import { PAY_SERVICE, COURSE_SERVICE, USER_SERVICE, NOTI_SERVICE } from './services.js'
 
 const app = express()
@@ -11,6 +11,7 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
 const PORT = process.env.PORT || 5000
+
 
 app.get('/', async (req, res) => {
     try {
@@ -24,22 +25,34 @@ app.get('/', async (req, res) => {
 app.use('/pay', createProxyMiddleware({
     target: PAY_SERVICE,
     changeOrigin: true,
+    on: {
+        proxyReq: fixRequestBody,
+    },
 }));
 
 
 app.use('/course', createProxyMiddleware({
     target: COURSE_SERVICE,
     changeOrigin: true,
+    on: {
+        proxyReq: fixRequestBody,
+    },
 }))
 
 app.use('/user', createProxyMiddleware({
     target: USER_SERVICE,
     changeOrigin: true,
+    on: {
+        proxyReq: fixRequestBody,
+    },
 }))
 
 app.use('/notification', createProxyMiddleware({
     target: NOTI_SERVICE,
     changeOrigin: true,
+    on: {
+        proxyReq: fixRequestBody,
+    },
 }))
 
 //Create other proxy addresses here
