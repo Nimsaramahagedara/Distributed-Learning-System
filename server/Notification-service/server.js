2450f17d7aa9edbba6260d1de7ed73a218d56147
YasithaRenuk <126389901+YasithaRenuk@users.noreply.github.com>
2024-05-13 16:09:29 +0530

    Adding comment to notification

--- server/Notification-service/server.js
+++ server/Notification-service/server.js
@@ -1,23 +1,22 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const dotenv = require("dotenv").config();
-const Notification = require("./routes/Notification");
+const express = require("express"); // Import Express framework
+const cors = require("cors"); // Import CORS middleware for enabling Cross-Origin Resource Sharing
+const bodyParser = require("body-parser"); // Import body-parser middleware for parsing JSON requests
+const dotenv = require("dotenv").config(); // Load environment variables from .env file
+const Notification = require("./routes/Notification"); // Import the router for handling notification routes
 
-const app = express();
+const app = express(); // Create an Express application instance
 
-const PORT = process.env.PORT || 8070;
+const PORT = process.env.PORT; // Define the port to listen on
 
-// app.use(cors({
-//   origin: 'http://localhost:5173', // Specify your frontend URL
-//   credentials: true // Allow sending cookies
-// }));
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // Parse JSON requests using body-parser middleware
 
+// Enable CORS for all routes
+app.use(cors());
 
 // Routes
-app.use("/", Notification);
-  
+app.use("/", Notification); // Mount the notification router at the root URL
+
+// Start the server, listen on the specified port
 app.listen(PORT, () => {
-  console.log(`Server is up and running on port ${PORT}`);
+  console.log(`Server is up and running on port ${PORT}`); // Log a message when the server starts
 });
