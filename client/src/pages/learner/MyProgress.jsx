09fb7d1e858650d8810beec652d8e8ce659b7317
nimsaramahagedara <nimsaramahagedara@gmail.com>
2024-05-15 00:00:10 +0530

    transactions-my-learning

--- client/src/pages/learner/MyProgress.jsx
+++ client/src/pages/learner/MyProgress.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import authAxios from '../../utils/authAxios';
+import { apiUrl } from '../../utils/Constants';
+import CourseCard from '../../components/Course/CourseCard';
 
 const MyProgress = () => {
+  const [myCourses, setMyCourses] = useState()
+  const getMyCourses =async()=>{
+    try {
+      const resp = await authAxios.get(`${apiUrl}/learn/all`)
+      setMyCourses(resp.data)
+      console.log(resp.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    getMyCourses()
+  },[])
   return (
-    <div>MyProgress</div>
+    <div>
+      {
+        myCourses?.map((c)=>(
+          <CourseCard course={c?.courseDetails} isEnrolled/>
+        ))
+      }
+    </div>
   )
 }
 
--- client/src/pages/learner/Transactions.jsx
+++ client/src/pages/learner/Transactions.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import authAxios from '../../utils/authAxios';
+import { apiUrl } from '../../utils/Constants';
 
 const Transactions = () => {
+  const [myTxs, setMyTx] = useState([])
+
+  const getMyTransactions = async () => {
+    try {
+      const resp = await authAxios.get(`${apiUrl}/pay/my`)
+      console.log(resp.data);
+      setMyTx(resp.data)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getMyTransactions()
+  }, [])
   return (
-    <div>Transactions</div>
+    <div>
+      <h1>My Transactions</h1>
+
+
+      <div>
+        {
+          myTxs.map((t) => (
+            <div className='flex items-center justify-between px-4 py-2 border my-2 bg-red-400 rounded-xl'>
+              <div>
+                {t.name}
+              </div>
+              <div className='text-right'>
+                {t.price}
+                <br />
+                <span className='text-xs text-gray-300'>{t?.createdAt ? new Date(t?.createdAt).toDateString() : new Date().toDateString()}</span>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
