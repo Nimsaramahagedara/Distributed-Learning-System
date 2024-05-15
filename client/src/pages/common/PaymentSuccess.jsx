27e2c755d3bcec594b6d38ec9490d44bbf81e1c0
YasithaRenuk <126389901+YasithaRenuk@users.noreply.github.com>
2024-05-15 13:23:59 +0530

    Notification add

--- client/src/pages/common/PaymentSuccess.jsx
+++ client/src/pages/common/PaymentSuccess.jsx
@@ -1,36 +1,94 @@
-import React, { useEffect, useState } from 'react'
-import done from '../../assets/done.svg'
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import done from "../../assets/done.svg";
+import { useNavigate } from "react-router-dom";
+import authAxios from "../../utils/authAxios";
+import { apiUrl } from "../../utils/Constants";
+import { toast } from "react-toastify";
+
 const PaymentSuccess = () => {
-    const [seconds, setSeconds] = useState(5);
-    const navigate = useNavigate()
+  const [seconds, setSeconds] = useState(5);
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setSeconds((prev) => {
-                if (prev <= 1) {
-                    clearInterval(intervalId);
-                    setTimeout(() => {
-                        navigate("/learner");
-                    }, 3000)
-                    navigate("/learner");
-                    return 0;
-                }
-                return prev - 1;
-            });
-        }, 1000);
+  const getUserDetails = async () => {
+    try {
+      const response = await authAxios.get(`${apiUrl}/user/loggedInUser`);
+      setUser(response.data);
+      sendSmsNotification(response.data); // Call sendSmsNotification after setUser
+    } catch (error) {
+      console.error(error);
+      if (error.response && error.response.status === 404) {
+        toast.error("user profile not found.");
+      } else {
+        // toast.error(error.response?.data?.message || 'An error occurred');
+      }
+    }
+  };
 
-        return () => {clearInterval(intervalId)};
-    }, [])
-    return (
-        <div className='w-full h-screen flex items-center justify-center'>
-            <div className='w-full md:w-1/2'>
-                <img src={done} className='w-full' />
-            </div>
-            <h1>Payment Success!</h1>
-            <p>You will redirect to the dashboard after {seconds} seconds</p>
-        </div>
-    )
-}
+  const sendSmsNotification = async (obj) => {
+    try {
+      const payload = {
+        to: "94"+obj.contactNo,
+        text: "Your payment was successful and you are enrolled in the course",
+      };
+      await authAxios.post(`${apiUrl}/notification/sendsms`, payload);
+    } catch (error) {
+      console.error(error);
+      if (error.response && error.response.status === 404) {
+        toast.error("user profile not found.");
+      } else {
+        // toast.error(error.response?.data?.message || 'An error occurred');
+      }
+    }
+  };
+  
 
-export default PaymentSuccess
+  const redirectToDashboard = () => {
+    navigate("/learner");
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await getUserDetails();
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSeconds((prev) => {
+        if (prev <= 1) {
+          clearInterval(intervalId);
+          setTimeout(() => {
+            redirectToDashboard();
+          }, 3000);
+          redirectToDashboard();
+          return 0;
+        }
+        return prev - 1;
+      });
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  
+
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className="w-full md:w-1/2">
+        <img src={done} className="w-full" alt="Payment Success" />
+      </div>
+      <h1>Payment Success!</h1>
+      <p>You will be redirected to the dashboard after {seconds} seconds</p>
+    </div>
+  );
+};
+
+export default PaymentSuccess;
